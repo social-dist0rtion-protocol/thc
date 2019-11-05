cf5c3a4088e11c09f745eb43ecb56967821020b0
vrde <agranzot@gmail.com>
2019-11-05 21:50:12 +0100

    fixeeees

--- eth/migrations/2_deploy_contracts.js
+++ eth/migrations/2_deploy_contracts.js
@@ -1,16 +1,18 @@
+const { readFileSync } = require("fs");
 const web3 = require("web3");
 const b58 = require("base-58");
-const CHAPTERS = require("./chapters.json");
 const THC = artifacts.require("./TreasureHuntCreator.sol");
 
 module.exports = function(deployer) {
+  const chapters = JSON.parse(readFileSync(process.argv.pop()));
+  console.log(chapters);
   var solutions = [];
   var quests = [];
 
-  CHAPTERS.map(chapter => {
-    var questHash = chapter["questHash"];
-    var decoded = b58.decode(questHash);
-    var quest = web3.utils.bytesToHex(decoded.slice(2));
+  chapters.map(chapter => {
+    const questHash = chapter["questHash"];
+    const decoded = b58.decode(questHash);
+    const quest = web3.utils.bytesToHex(decoded.slice(2));
     quests.push(quest);
     solutions.push(chapter["solutionAddress"]);
   });
