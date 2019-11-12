85d410b2717ff96bd46b0ceae55d769064970eda
Nicola Miotto <sirnicolaz@gmail.com>
2019-11-12 01:08:09 +0100

    fix env

--- gen/push_client.js
+++ gen/push_client.js
@@ -1,14 +1,16 @@
 const ipfsClient = require("ipfs-http-client");
 
 const PATH = process.argv[2];
-
-import NETWORK_CONFIG from "network_config.json";
+const IPFS_PROTOCOL = process.env["IPFS_PROTCOL"];
+const IPFS_HOST = process.env["IPFS_HOST"];
+const IPFS_PORT = process.env["IPFS_PORT"];
+const IPFS_LOCATION = process.env["IPFS_LOCATION"];
 
 async function upload(path) {
   const ipfs = ipfsClient({
-    host: NETWORK_CONFIG["IPFS_HOST"],
-    port: NETWORK_CONFIG["IPFS_PORT"],
-    protocol: NETWORK_CONFIG["IPFS_PROTOCOL"]
+    host: IPFS_HOST,
+    port: IPFS_PORT,
+    protocol: IPFS_PROTOCOL
   });
 
   const options = {
@@ -19,7 +21,7 @@ async function upload(path) {
   try {
     result = await ipfs.addFromFs(path, options);
     console.log(result);
-    let url = NETWORK_CONFIG["IPFS_LOCATION"] + result.pop().hash;
+    let url = IPFS_LOCATION + result.pop().hash;
     console.log(url);
   } catch (err) {
     console.log(err);
