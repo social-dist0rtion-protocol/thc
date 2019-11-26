5c7683ad9a8a4c5c829a82541ec293bd657fdfaa
Nicola Miotto <sirnicolaz@gmail.com>
2019-11-26 22:58:57 +0100

    validity checks

--- srv/app.js
+++ srv/app.js
@@ -6,8 +6,21 @@ const port = 3000;
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.get("/tokens", async function(req, res) {
-  let hash = await tokens.send(req.query.address, process.env.AMOUNT);
-  res.send(hash);
+  try {
+    let address = req.query.address;
+    let amount = process.env.AMOUNT;
+    let hash = await tokens.send(address, amount);
+    res.send(
+      "Amount " +
+        amount +
+        " transferred to " +
+        address +
+        "<br>Transaction hash " +
+        hash
+    );
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
