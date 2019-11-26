8c77a2bfe0a88245b6fc6f34373ab61a44aa59bd
Nicola Miotto <sirnicolaz@gmail.com>
2019-11-26 22:58:57 +0100

    validity checks

--- srv/tokens.js
+++ srv/tokens.js
@@ -3,20 +3,25 @@ const URI = "https://goerli.infura.io/v3/322fadda61904e8c937489c2e3ad6387";
 
 module.exports = {
   send: async function send(to, value) {
-    let wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC);
-    let provider = new ethers.providers.JsonRpcProvider(URI);
-    wallet = wallet.connect(provider);
-
-    let tx = {
-      to,
-      value: ethers.utils.parseEther(value)
-    };
-
     try {
-      let transaction = await wallet.sendTransaction(tx);
-      return transaction.hash;
+      ethers.utils.getAddress(to);
+      let wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC);
+      let provider = new ethers.providers.JsonRpcProvider(URI);
+      wallet = wallet.connect(provider);
+
+      let tx = {
+        to,
+        value: ethers.utils.parseEther(value)
+      };
+
+      try {
+        let transaction = await wallet.sendTransaction(tx);
+        return transaction.hash;
+      } catch (err) {
+        throw "Transaction failed";
+      }
     } catch (err) {
-      console.log("Sending wasn't successful", err);
+      throw "Invalid address";
     }
   }
 };
