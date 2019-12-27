0210fa5e375bdbfbedc3baa138d4e72fabbbdcf6
vrde <agranzot@gmail.com>
2019-12-27 21:43:33 +0100

    Fix leaderboard

--- app/src/Leaderboard.svelte
+++ app/src/Leaderboard.svelte
@@ -2,8 +2,17 @@
   import { onMount } from "svelte";
   import { wallet, currentQuest, current, provider, thc } from "./stores";
   import ethers from "ethers";
+  import BN from "bn.js";
 
   async function load() {
+    function parse(value) {
+      const v = new BN(value.toString(), 10);
+      const address = v.shrn(96).toString(16);
+      const chapter = v.maskn(96).toNumber();
+      const shortAddress =
+        "0x" + address.substr(0, 4) + "…" + address.substr(16, 4);
+      return { address: "0x" + address, shortAddress, chapter };
+    }
     async function getLeaderboard() {
       const ZERO = ethers.utils.bigNumberify(0);
       const leaderboard = [];
@@ -12,23 +21,19 @@
         const items = await $thc.functions.getLeaderboard(page);
         for (let item of items) {
           const value = ethers.utils.bigNumberify(item);
-          console.log(value);
           if (value.eq(ZERO)) {
             return leaderboard;
           } else {
-            leaderboard.push(value);
+            leaderboard.push(parse(value));
           }
         }
         page++;
       }
     }
 
-    function parse(value) {
-      const address = value.shrn(96);
-      //const chapter = value.
-    }
-
-    return getLeaderboard();
+    const leaderboard = await getLeaderboard();
+    leaderboard.sort((a, b) => b.chapter - a.chapter);
+    return leaderboard;
   }
   load();
 </script>
@@ -42,11 +47,27 @@
 {#await load()}
   <p>Loading...</p>
 {:then values}
-  <ol>
-    {#each values as value}
-      <li>{value}</li>
-    {/each}
-  </ol>
+  <table class="leaderboard">
+    <thead>
+      <tr>
+        <td>Address</td>
+        <td>Chapter</td>
+      </tr>
+    </thead>
+    <tbody>
+      {#each values as value}
+        <tr>
+          <td>
+            {value.shortAddress}
+            {#if value.address.toLowerCase() === $wallet.address.toLowerCase()}
+              <span class="label hope">that's you</span>
+            {/if}
+          </td>
+          <td>{value.chapter}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
 {:catch error}
   <p>something went wrong. "{error.message}"</p>
 {/await}
