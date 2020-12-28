31ae184adb2a055f12e2483bf62abf30a4567650
vrde <agranzot@mailbox.org>
2020-12-28 16:42:50 +0100

    Use private key instead of mnemonic

--- srv/tokens.js
+++ srv/tokens.js
@@ -1,19 +1,17 @@
 const ethers = require("ethers");
-const URI =
-  process.env.ETH_ENDPOINT ||
-  "https://goerli.infura.io/v3/322fadda61904e8c937489c2e3ad6387";
+const URI = process.env.ETH_ENDPOINT;
 
 module.exports = {
   send: async function send(to, value) {
     try {
       ethers.utils.getAddress(to);
-      let wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC);
+      let wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
       let provider = new ethers.providers.JsonRpcProvider(URI);
       wallet = wallet.connect(provider);
 
       let tx = {
         to,
-        value: ethers.utils.parseEther(value)
+        value: ethers.utils.parseEther(value),
       };
 
       try {
@@ -25,5 +23,5 @@ module.exports = {
     } catch (err) {
       throw "Invalid address";
     }
-  }
+  },
 };
