875e2f376f6e40eb368d5e4489b8a40846738b88
vrde <agranzot@mailbox.org>
2020-12-29 16:50:29 +0100

    add logging

--- srv/app.js
+++ srv/app.js
@@ -5,11 +5,12 @@ const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get("/tokens", async function(req, res) {
+app.get("/tokens", async function (req, res) {
   try {
     let address = req.query.address;
     let amount = process.env.AMOUNT;
     let hash = await tokens.send(address, amount);
+    console.log("Amount " + amount + " → " + address + " tx " + hash);
     res.send(
       "Amount " +
         amount +
