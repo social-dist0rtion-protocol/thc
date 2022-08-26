f61d58e3c640695acf1ba1e73456f9efede5530a
Nicola Miotto <miotto@posteo.de>
2022-08-26 13:19:17 +0200

    fix deploy script

--- eth/hardhat.config.ts
+++ eth/hardhat.config.ts
@@ -13,11 +13,8 @@ import "solidity-coverage";
 import("./tasks").catch((e) => console.log("Cannot load tasks", e.toString()));
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
-const RINKEBY_PRIVATE_KEY =
+const GOERLI_PRIVATE_KEY =
   process.env.RINKEBY_PRIVATE_KEY! ||
-  "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
-const KOVAN_PRIVATE_KEY =
-  process.env.KOVAN_PRIVATE_KEY! ||
   "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const COINMARKETCAP_KEY = process.env.COINMARKETCAP_KEY || "";
@@ -40,13 +37,9 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {},
     localhost: {},
-    kovan: {
-      url: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [KOVAN_PRIVATE_KEY],
-    },
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [RINKEBY_PRIVATE_KEY],
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
     },
   },
   etherscan: {
