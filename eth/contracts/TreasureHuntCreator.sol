5dba941a764215bf29d9cc87acba823aae750aba
vrde <agranzot@mailbox.org>
2022-10-04 13:48:22 +0200

    Use OZ roles

--- eth/contracts/TreasureHuntCreator.sol
+++ eth/contracts/TreasureHuntCreator.sol
@@ -1,52 +1,38 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.8.12 <0.9.0;
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TreasureHuntCreator is Ownable {
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract TreasureHuntCreator is Ownable, AccessControl {
+    bytes32 public constant GAME_MASTER_ROLE = keccak256("GAME_MASTER_ROLE");
+
     event ChapterCompleted(
-        uint indexed completedChapter,
+        uint256 indexed completedChapter,
         address indexed player
     );
 
-    uint constant PAGE_SIZE = 32;
+    uint256 constant PAGE_SIZE = 32;
 
     mapping(uint96 => address[]) public _chapterToPlayers;
     mapping(address => uint96) public _playerToCurrentChapter;
     address[] public _solutions;
     address[] public _players;
     address[] public _gameMasters;
-    
+
     bytes public _questsRootCid;
 
     constructor(address[] memory solutions, bytes memory questsRootCid) {
         _solutions = solutions;
         _questsRootCid = questsRootCid;
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(GAME_MASTER_ROLE, msg.sender);
     }
 
-    modifier onlyGameMaster() {
-        require(isGameMaster(), "Only game masters can use this function.");
-        _;
-    }
-
-    modifier onlyPlayer() {
-        require(
-            _playerToCurrentChapter[msg.sender] >= 1,
-            "Player did not join yet. Call 'join' first"
-        );
-        _;
-    }
-
-    function isGameMaster() internal view returns (bool) {
-        for (uint i; i < _gameMasters.length; i++) {
-            if (_gameMasters[i] == msg.sender) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function setQuestsRootCID(bytes memory questsRootCid) external onlyGameMaster {
+    function setQuestsRootCID(bytes memory questsRootCid)
+        external
+        onlyRole(GAME_MASTER_ROLE)
+    {
         _questsRootCid = questsRootCid;
     }
 
@@ -54,25 +40,11 @@ contract TreasureHuntCreator is Ownable {
         return _questsRootCid;
     }
 
-    function addSolution(address solution)
-        public
-        onlyGameMaster
-    {
+    function addSolution(address solution) public onlyRole(GAME_MASTER_ROLE) {
         _solutions.push(solution);
     }
 
-    function addGameMaster(address gameMaster) public onlyOwner {
-        for (uint i = 0; i < _gameMasters.length; i++) {
-            require(
-                _gameMasters[i] != gameMaster,
-                "This game master has already been added"
-            );
-        }
-
-        _gameMasters.push(gameMaster);
-    }
-
-    function totalChapters() public view returns (uint) {
+    function totalChapters() public view returns (uint256) {
         return _solutions.length;
     }
 
@@ -107,13 +79,17 @@ contract TreasureHuntCreator is Ownable {
             keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n20", a));
     }
 
-    function getLeaderboard(uint page)
+    function getLeaderboard(uint256 page)
         public
         view
         returns (uint256[PAGE_SIZE] memory leaderboard)
     {
-        uint offset = page * PAGE_SIZE;
-        for (uint i = 0; i < PAGE_SIZE && i + offset < _players.length; i++) {
+        uint256 offset = page * PAGE_SIZE;
+        for (
+            uint256 i = 0;
+            i < PAGE_SIZE && i + offset < _players.length;
+            i++
+        ) {
             address player = _players[i + offset];
 
             leaderboard[i] =
