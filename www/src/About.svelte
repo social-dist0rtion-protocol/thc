3d42ad11850003ff59bd4dcd9d45d0c85438e564
vrde <agranzot@mailbox.org>
2022-10-10 22:46:15 +0200

    Add faq

--- www/src/About.svelte
+++ www/src/About.svelte
@@ -1,11 +1,55 @@
-<h1>About</h1>
+<h1>FAQ</h1>
 
+<h3>Where do I get Görli Ether?</h3>
+<p>
+  Get it from a faucet, or come to our <a
+    href="https://discord.com/invite/GMU7j2kqWQ">discord server</a
+  > and ask for some Göerli Ether.
+</p>
+
+<h3>What are keys and where do I get them?</h3>
+<p>
+  Keys are hidden mnemonic phrases hidden in the game. If you find a key, you
+  can claim it in the "keys" section of the game. Insert the mnemonic in the
+  input box and submit it. If it's correct it will appear in the leaderboard
+  next to your address.
+</p>
+
+<h3>I'm stuck, can I get a hint?</h3>
+<p>
+  Join our <a href="https://discord.com/invite/GMU7j2kqWQ">discord server</a>,
+  we have a channel where you can ask for help.
+</p>
+
+<h3>Why should I register an ENS name?</h3>
+<p>
+  Addresses are cool but they are very difficult to pronounce and remember.
+  Worry not, if you register an ENS name <strong>on Görli</strong> you can
+  customize your name and avatar in the leadeboard! How cool is that!
+  <small>
+    (The real reason to do it it's because we spent quite some time to integrate
+    ENS in this app so please show some appreciation to our overly stressed devs
+    and create an ENS entry and add a avatar image as well but please don't use
+    the <code>ipfs://</code> schema or fancy NFT profile pics it would be cool ofc
+    we are all here for decentralization but we didn't have enough time to integrate
+    this PRs WELCOME!)
+  </small>
+</p>
+
+<h3>How do I register my ENS name?</h3>
+<p>
+  Export your mnemonic to another wallet, go to the ENS domain and register your
+  name in the <strong>Görli network</strong>. You can also add an avatar (we
+  only support `http` and `https` schemes).
+</p>
+
+<h3>I want to talk with the manager</h3>
 <p>
   <strong>No Time to DAI</strong> is a treasure hunt challenge (THC for short)
   created by
   <a href="https://www.dist0rtion.com">Social Dist0rtion Protocol</a> and
-  <a href="http://daedalus.industries/">daedalus industries</a> (check their
-  website, it's amazing!) for <a href="https://ethberlin.ooo/">EthBerlin3</a>.
+  <a href="http://daedalus.industries/">daedalus industries</a> for
+  <a href="https://devcon.org/">DevCon6</a>.
 </p>
 
 <p>If you get stuck during the game you can reach us:</p>
