5c0268ce435ef8ccdae407788623ab1adbbaa91c
vrde <agranzot@mailbox.org>
2022-10-11 14:50:48 +0200

    Improve faq

--- www/src/About.svelte
+++ www/src/About.svelte
@@ -2,14 +2,18 @@
 
 <h3>Where do I get Görli Ether?</h3>
 <p>
-  Get it from a faucet, or come to our <a
-    href="https://discord.com/invite/GMU7j2kqWQ">discord server</a
-  > and ask for some Göerli Ether.
+  Get it from a faucet (<a href="https://goerlifaucet.com/">1</a>,
+  <a href="https://goerli-faucet.mudit.blog/">2</a>,
+  <a href="https://goerli-faucet.pk910.de/">3</a>,
+  <a href="https://faucet.paradigm.xyz/">4</a>). If you are lazy you can also
+  come to our
+  <a href="https://discord.gg/RgJdhMx8PR">discord server</a> and ask for some Göerli
+  Ether in the "💸 I need Goerli Eth" channel. Remember to share your wallet address.
 </p>
 
 <h3>What are keys and where do I get them?</h3>
 <p>
-  Keys are hidden mnemonic phrases hidden in the game. If you find a key, you
+  Keys are 24-word mnemonic phrases hidden in the game. If you find a key, you
   can claim it in the "keys" section of the game. Insert the mnemonic in the
   input box and submit it. If it's correct it will appear in the leaderboard
   next to your address.
@@ -17,8 +21,8 @@
 
 <h3>I'm stuck, can I get a hint?</h3>
 <p>
-  Join our <a href="https://discord.com/invite/GMU7j2kqWQ">discord server</a>,
-  we have a channel where you can ask for help.
+  Join our <a href="https://discord.gg/cxZxcu7MN7">discord server</a>, we have a
+  channel where you can ask for help called "🆘 I need help".
 </p>
 
 <h3>Why should I register an ENS name?</h3>
@@ -27,12 +31,13 @@
   Worry not, if you register an ENS name <strong>on Görli</strong> you can
   customize your name and avatar in the leadeboard! How cool is that!
   <small>
-    (The real reason to do it it's because we spent quite some time to integrate
+    (The real reason to do it is because we spent quite some time to integrate
     ENS in this app so please show some appreciation to our overly stressed devs
-    and create an ENS entry and add a avatar image as well but please don't use
-    the <code>ipfs://</code> schema or fancy NFT profile pics it would be cool ofc
-    we are all here for decentralization but we didn't have enough time to integrate
-    this PRs WELCOME!)
+    and create an ENS entry and add an avatar image as well <strike>
+      but please don't use the <code>ipfs://</code> schema or fancy NFT profile pics
+      it would be cool ofc we are all here for decentralization but we didn't have
+      enough time to integrate this PRs WELCOME!
+    </strike> and you can use `ipfs://` and `bzz://`!)
   </small>
 </p>
 
@@ -43,32 +48,26 @@
   only support `http` and `https` schemes).
 </p>
 
-<h3>I want to speak with the manager</h3>
+<h3>I want to speak to the manager</h3>
 <p>
   <strong>No Time to DAI</strong> is a treasure hunt challenge (THC for short)
   created by
   <a href="https://www.dist0rtion.com">Social Dist0rtion Protocol</a> and
   <a href="http://daedalus.industries/">daedalus industries</a> for
-  <a href="https://devcon.org/">DevCon6</a>.
+  <a href="https://devcon.org/">DevCon6</a>. We do this for the clout so don't
+  forget to follow on tweeeeetererer:
+  <a href="https://twitter.com/dist0rtionproto">@dist0rtionporoto</a>
 </p>
 
-<p>If you get stuck during the game you can reach us:</p>
+<h3>Show me the code</h3>
+<p>
+  The Holy Code of Treasure Hunt Creator (or THCTHC for short) is published on <a
+    href="https://github.com/social-dist0rtion-protocol/thc">GitHub</a
+  >.
+</p>
 
-<ul>
-  <li>
-    On twitter, DM <a href="https://twitter.com/dist0rtionproto"
-      >@dist0rtionporoto</a
-    >
-  </li>
-  <li>
-    On discord, join our server <a href="https://discord.gg/GMU7j2kqWQ"
-      >No Time To DAI</a
-    >
-  </li>
-  <li>
-    If you are brave enough you can also interact with us in person, but we are
-    not sure how you can recognize us since we are not famous people (yet!). But
-    we believe in your skills and we are sure you'll be able to get in touch
-    with us :)
-  </li>
-</ul>
+<style>
+  h3 {
+    margin-top: 3rem;
+  }
+</style>
