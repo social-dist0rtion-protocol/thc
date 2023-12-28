20e86609c10acbdf1cfd09135ca6c15148df03c2
Alberto Granzotto <alberto@granzotto.net>
2023-12-28 14:29:17 +0100

    add cors dunno why it's a get

--- srv/app.js
+++ srv/app.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const tokens = require("./tokens");
+var cors = require("cors");
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.get("/tokens", async function (req, res) {
