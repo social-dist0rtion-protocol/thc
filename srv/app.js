f2bd924a13df361773b55a189bc0eb06b2ef4fc1
Tim Daubenschütz <tim@daubenschuetz.de>
2023-12-28 16:35:02 +0100

    Add cors support

--- srv/app.js
+++ srv/app.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const tokens = require("./tokens");
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.get("/tokens", async function (req, res) {
