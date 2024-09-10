2cdd55e0fd4d0d9f1552191d31b69ccfed8d70d7
Alberto Granzotto <alberto@granzotto.net>
2024-09-10 17:30:24 +0200

    Expose func to make wallet out of a solution

--- lib/src/thc.ts
+++ lib/src/thc.ts
@@ -2,8 +2,12 @@ import { keccak256, toHex } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { stringToUint8Array } from "./utils";
 
-export function addressFromSolution(solution: string) {
+export function walletFromSolution(solution: string) {
   const solutionRaw = stringToUint8Array(solution.trim().toLowerCase());
   const solutionHash = keccak256(toHex(solutionRaw));
-  return privateKeyToAccount(solutionHash).address;
+  return privateKeyToAccount(solutionHash);
+}
+
+export function addressFromSolution(solution: string) {
+  return walletFromSolution(solution).address;
 }
