e5e1086ca46f827d27f5f3ef6c2e9c07a7d325e2
vrde <alberto@granzotto.net>
2024-09-20 13:52:12 +0200

    Add withdraw function (#73)
    * Add withdraw function
    Fixes #72
    
    * Add method to withdraw native token

---
+++ eth/contracts/mocks/MyToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("MyToken", "MTK") {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
