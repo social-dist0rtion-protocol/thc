757e400695af2bff9b1e599f820a2a7951d9114a
Nicola Miotto <miotto@posteo.de>
2024-09-20 18:26:20 +0200

    add missing file

---
+++ web/src/hooks/storage.tsx
@@ -0,0 +1,20 @@
+export const MNEMONIC_KEY = "mnemonic";
+export const CURRENT_CHAPTER_INDEX_KEY = "currentChapterIndex";
+export const CURRENT_CHAPTER_PASSWORD_KEY = "currentChapterPassword";
+export const CURRENT_KEY_PASSWORD_KEY = "currentKeyPassword";
+
+export function dump() {
+  let theDump: Record<string, string> = {};
+  for (let key in localStorage) {
+    theDump[key] = localStorage[key];
+  }
+
+  return JSON.stringify(theDump);
+}
+
+export function restore(theDump: string) {
+  const dumpObj = JSON.parse(theDump);
+  for (let key in dumpObj) {
+    localStorage.setItem(key, dumpObj[key]);
+  }
+}
