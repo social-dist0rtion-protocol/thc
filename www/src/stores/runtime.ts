e9c880590ef06f2592dd8c471f2742c6a0409e6b
Nicola Miotto <miotto@posteo.de>
2024-09-20 15:13:53 +0200

    delete www

--- www/src/stores/runtime.ts
+++
@@ -1,8 +0,0 @@
-import { writable } from "svelte/store";
-
-export const panic = writable(false);
-
-window.addEventListener("unhandledrejection", function (e) {
-  console.error("Unhandled Rejection", e);
-  panic.set(true);
-});
