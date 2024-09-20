05e30685995bcb0a68ce3bd9c35dbd562ffbd2f1
Nicola Miotto <miotto@posteo.de>
2024-09-20 15:38:30 +0200

    default fee token

--- web/src/env.tsx
+++ web/src/env.tsx
@@ -5,6 +5,6 @@ export const MAP_URL = import.meta.env.VITE_MAP_URL;
 export const HELP_URL = import.meta.env.VITE_HELP_URL;
 export const RPC_NODE_URL = import.meta.env.VITE_RPC_NODE_URL;
 export const CHAIN_ID = thc.chainId;
-export const GELATO_FEE_TOKEN = import.meta.env.VITE_GELATO_FEE_TOKEN;
+export const GELATO_FEE_TOKEN = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
 export const WALLET_CONNECT_PROJECT_ID = import.meta.env
   .VITE_WALLET_CONNECT_PROJECT_ID;
