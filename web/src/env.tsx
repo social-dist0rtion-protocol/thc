2381f9601d8314ca25f346ac7473abf65094f155
Nicola Miotto <miotto@posteo.de>
2024-09-20 15:46:43 +0200

    move env variables to thc

--- web/src/env.tsx
+++ web/src/env.tsx
@@ -1,10 +1,10 @@
 import thc from "../thc.json";
 
-export const APP_NAME = import.meta.env.VITE_APP_NAME;
-export const MAP_URL = import.meta.env.VITE_MAP_URL;
-export const HELP_URL = import.meta.env.VITE_HELP_URL;
-export const RPC_NODE_URL = import.meta.env.VITE_RPC_NODE_URL;
+export const APP_NAME = thc.name;
+export const MAP_URL = thc.mapUrl;
+export const HELP_URL = thc.helpUrl;
 export const CHAIN_ID = thc.chainId;
 export const GELATO_FEE_TOKEN = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
+export const RPC_NODE_URL = import.meta.env.VITE_RPC_NODE_URL;
 export const WALLET_CONNECT_PROJECT_ID = import.meta.env
   .VITE_WALLET_CONNECT_PROJECT_ID;
--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -1,8 +1,5 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
-  readonly VITE_APP_NAME: string;
-  readonly VITE_MAP_URL: string;
-  readonly VITE_HELP_URL: string;
   readonly VITE_RPC_NODE_URL: string;
   readonly VITE_WALLET_CONNECT_PROJECT_ID: string;
 }
