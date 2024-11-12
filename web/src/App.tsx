4c541c000e9c8cc938e2a95ec4c7fa57f625e70e
Nicola Miotto <miotto@posteo.de>
2024-11-12 06:20:28 +0100

    add undercontstruction page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,7 @@
-import { Box, Center, Heading, Image, VStack } from "@chakra-ui/react";
+import { Box, Center, VStack } from "@chakra-ui/react";
+import HeaderMenu from "./HeaderMenu";
 import useIsMobile from "./hooks/useIsMobile";
+import { Outlet } from "react-router-dom";
 import OfflineAlert from "./OfflineAlert";
 
 function App() {
@@ -10,16 +12,15 @@ function App() {
       <OfflineAlert />
       <Center>
         <VStack width="100%">
+          <HeaderMenu />
           <Box
-            textAlign="center"
             backgroundColor="transparent"
             marginTop="20px"
             marginBottom="60px"
             width={isMobile ? "95%" : "40%"}
             layerStyle="base"
           >
-            <Heading>Under Construction</Heading>
-            <Image src="under.png" />
+            <Outlet />
           </Box>
         </VStack>
       </Center>
