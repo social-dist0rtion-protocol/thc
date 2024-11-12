a1bf3f36e770ebfa7acac256394c7f45f84e247a
Alberto Granzotto <alberto@granzotto.net>
2024-11-12 20:05:47 +0700

    Update FAQ

--- web/src/FAQ.tsx
+++ web/src/FAQ.tsx
@@ -7,7 +7,7 @@ import {
   Text,
   VStack,
 } from "@chakra-ui/react";
-import { CONTRACT_ADDRESS } from "./env";
+import { HELP_URL } from "./env";
 
 function FAQ() {
   return (
@@ -18,36 +18,44 @@ function FAQ() {
         <List>
           <ListItem>
             We made this game for people to haffun, if you need a bit of help to
-            get going with your THC (Treasure Hunt Challenge) join our Matrix
-            channel, or visit us at, we are in the third floor (check the map!).
+            get going with your THC (Treasure Hunt Challenge){" "}
+            <Link href={HELP_URL} target="_blank">
+              join our Discord server
+            </Link>
           </ListItem>
           <ListItem>
-            Or write us on twitter, we will do our best to be responsive.
+            Or write us on{" "}
+            <Link href="https://x.com/dist0rtionproto" isExternal>
+              X
+            </Link>
+            , we will do our best to be responsive.
           </ListItem>
         </List>
       </Box>
       <Box layerStyle="base">
         <Heading variant="h3">What are the "side quests"</Heading>
         <Text>
-          The side quests are optional puzzles. All documents start with the
-          hashtag symbol #. They may require specific technical skills. Don't
-          expect them to be easy. Actually, we really hope all of them will be
-          solved at one point, we put so much sweat 😓 on them (figuratively).
-          You don't need to do them, but you earn extra points if you complete
-          them.
+          The side quests are optional puzzles. They may require specific
+          technical skills. Each key in the side quest resembles a seed phrase.
+          Don't expect them to be easy. Actually, we really hope all of them
+          will be solved at one point, we put so much sweat 😓 on them
+          (figuratively). You don't need to do them, but you earn extra points
+          if you complete them.
         </Text>
       </Box>
       <Box layerStyle="base">
         <Heading variant="h3">I want to speak to the manager</Heading>
         <Text>
-          {CONTRACT_ADDRESS} is a Treasure Hunt Challenge (THC for short)
-          created by
-          <Link href="https://dist0rtion.com" isExternal>
-            Social Dist0rtion Protocol
+          is a Treasure Hunt Challenge (THC for short) created by
+          <Link href="https://dist0rtion.com/" isExternal>
+            Social Dist0rtion Protocol (SDP)
+          </Link>{" "}
+          and
+          <Link href="https://dedalus.industries/" isExternal>
+            daedalus.industries
           </Link>
-          . We do this for the clout so don't forget to follow on
-          tweeeeetererer:{" "}
-          <Link href="https://dist0rtion.com/dist0rtionproto" isExternal>
+          . If you feel social follow SDP on X:
+          <Link href="https://x.com/dist0rtionproto" isExternal>
             @dist0rtionporoto
           </Link>
         </Text>
