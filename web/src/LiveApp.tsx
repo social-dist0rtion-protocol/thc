9c439d33a4ade8dd1176c58a4d720c6344d64512
Nicola Miotto <miotto@posteo.de>
2024-11-12 04:23:48 +0100

    under contstruction

--- web/src/LiveApp.tsx
+++ web/src/LiveApp.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Heading, Image, VStack } from "@chakra-ui/react";
+import { Box, Center, VStack } from "@chakra-ui/react";
 import HeaderMenu from "./HeaderMenu";
 import useIsMobile from "./hooks/useIsMobile";
 import { Outlet } from "react-router-dom";
@@ -12,16 +12,15 @@ function App() {
       <OfflineAlert />
       <Center>
         <VStack width="100%">
+          <HeaderMenu />
           <Box
-            textAlign="center"
             backgroundColor="transparent"
             marginTop="20px"
             marginBottom="60px"
             width={isMobile ? "95%" : "40%"}
             layerStyle="base"
           >
-            <Heading>Under Construction</Heading>
-            <Image src="under.png" />
+            <Outlet />
           </Box>
         </VStack>
       </Center>
